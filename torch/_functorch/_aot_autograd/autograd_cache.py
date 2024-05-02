096fe0af9d525380a13ab883988484b6dd932110
James Wu <jjwu@meta.com>
2024-05-02 08:59:57 -0700

    Implement serialization and deserialization for fx graphs produced by AOTAutograd
    ghstack-source-id: 310a88af38bb5c04b8287f8964e3195e2fa8744a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125157

--- torch/_functorch/_aot_autograd/autograd_cache.py
+++ torch/_functorch/_aot_autograd/autograd_cache.py
@@ -3,18 +3,28 @@ Utils for caching the outputs of AOTAutograd
 """
 from __future__ import annotations
 
+import dataclasses
+
 import functools
 import logging
+import operator
 import os
+from typing import Any, cast, Dict, List
 
 import torch
+from torch._guards import detect_fake_mode
 from torch._inductor.codecache import (
     _ident,
     FxGraphCachePickler,
     get_code_hash,
     get_inductor_root,
 )
-from torch.fx.node import Node
+from torch._subclasses.fake_tensor import (
+    extract_tensor_metadata,
+    FakeTensor,
+    TensorMetadata,
+)
+from torch.fx.node import _get_qualified_name, Node
 
 from .schemas import AOTConfig  # noqa: F401
 
@@ -23,6 +33,20 @@ log = logging.getLogger(__name__)
 
 class BypassAOTAutogradCache(Exception):
     pass
+
+
+def fake_tensor_from_meta(tensor_meta):
+    fake_mode = detect_fake_mode()
+    with fake_mode:
+        return cast(
+            FakeTensor,
+            torch.empty_strided(
+                tensor_meta.shape,
+                tensor_meta.stride,
+                dtype=tensor_meta.dtype,
+                requires_grad=tensor_meta.requires_grad,
+            ),
+        )
 
 
 def check_node_safe(node: Node):
@@ -137,3 +161,164 @@ def autograd_cache_hash(
     key = "a" + AOTAutogradCachePickler.get_hash(details)
     log.debug("FX graph cache hash details for key %s:\n%s", key, details.debug_str())
     return key
+
+
+@dataclasses.dataclass
+class SerializedAOTGraphModule:
+    """
+    Object to be pickled by AOTAutogradCache.
+    We have the invariant that serializing and deserializing a graph module with this
+    class will preserve `old_module.print_readable() == new_module.print_readable()`
+    """
+
+    module: torch.fx.GraphModule
+    node_metas: Dict[str, Any]
+    node_names: List[str]
+
+
+class NodeMetaSerializer:
+    """
+    Convert a node's meta field into a pickleable dictionary and back
+    """
+
+    # Fields that are already serializable and therefore don't need a special implementation
+    SERIALIZABLE_FIELDS = ["tensor_meta", "stack_trace", "seq_nr"]
+
+    # Serialize or deserialize implementation
+    @staticmethod
+    def _convert(node_meta, action):
+        new_meta = {}
+        for key in node_meta:
+            try:
+                if key in NodeMetaSerializer.SERIALIZABLE_FIELDS:
+                    new_meta[key] = node_meta[key]
+                else:
+                    new_meta[key] = getattr(NodeMetaSerializer, f"{action}_{key}")(
+                        node_meta[key]
+                    )
+            except AttributeError as e:
+                raise BypassAOTAutogradCache(
+                    f"No serialization implemented for meta field {key} with value {node_meta[key]}."
+                    "Implement serialize_{key} and deserialize_{key}"
+                ) from e
+            except Exception as e:
+                raise BypassAOTAutogradCache(
+                    f"Failed serializing meta field {key}"
+                ) from e
+        return new_meta
+
+    @staticmethod
+    def serialize(node_meta):
+        return NodeMetaSerializer._convert(node_meta, "serialize")
+
+    @staticmethod
+    def deserialize(serialized_meta):
+        return NodeMetaSerializer._convert(serialized_meta, "deserialize")
+
+    @staticmethod
+    def serialize_val(val):
+        # TODO: handle other possible values
+        if not isinstance(val, torch.Tensor):
+            return val
+        return extract_tensor_metadata(val)
+
+    @staticmethod
+    def serialize_operator(op):
+        if isinstance(op, str):
+            return op
+        elif hasattr(op, "__module__"):
+            return _get_qualified_name(op)
+        else:
+            # TODO: handle other possible cases
+            raise BypassAOTAutogradCache(f"Don't know how to serialize operator {op}")
+
+    @staticmethod
+    def deserialize_operator(serialized_target: str):
+        # Weird case from _export/serde/serialize.py
+        if serialized_target.startswith("_operator"):
+            module = operator
+            serialized_target_names = serialized_target.split(".")[1:]
+        elif serialized_target.startswith("torch.nn"):
+            module = torch.nn
+            serialized_target_names = serialized_target.split(".")[2:]
+        elif serialized_target.startswith("torch"):
+            module = torch  # type: ignore[misc]
+            serialized_target_names = serialized_target.split(".")[1:]
+        else:
+            return serialized_target
+
+        target = module
+        for name in serialized_target_names:
+            if not hasattr(target, name):
+                return serialized_target
+            else:
+                target = getattr(target, name)
+        return target
+
+    @staticmethod
+    def serialize_source_fn_stack(source_fn_stack):
+        return [
+            (name, f"{NodeMetaSerializer.serialize_operator(target)}")
+            for (name, target) in source_fn_stack
+        ]
+
+    @staticmethod
+    def deserialize_source_fn_stack(source_fn_stack):
+        return [
+            (name, NodeMetaSerializer.deserialize_operator(target))
+            for (name, target) in source_fn_stack
+        ]
+
+    """ TODO: implement serialization for these fields """
+
+    @staticmethod
+    def serialize_original_aten(original_aten):
+        return NodeMetaSerializer.serialize_operator(original_aten)
+
+    @staticmethod
+    def deserialize_original_aten(original_aten):
+        return NodeMetaSerializer.deserialize_operator(original_aten)
+
+    @staticmethod
+    def serialize_from_node(val):
+        return NodeMetaSerializer.serialize_source_fn_stack(val)
+
+    @staticmethod
+    def deserialize_from_node(val):
+        return NodeMetaSerializer.deserialize_source_fn_stack(val)
+
+    @staticmethod
+    def deserialize_val(val):
+        # TODO: handle other possible values
+        if not isinstance(val, TensorMetadata):
+            return val
+        return fake_tensor_from_meta(val)
+
+
+def serialize_graph_module(module: torch.fx.GraphModule) -> SerializedAOTGraphModule:
+    """Converts a graph module to a pickeable format while preserving meta fields.
+    When copying a graph module, node names and meta fields aren't necessarily preserved,
+    but we want to make sure they're equivalent in our serialization, so we copy the results.
+    To do so, we need to preserve the node names and serialized meta fields in the serialized object.
+    Similarly, the class name is reset on any module copy, so we save the original class name on the module.
+    """
+    node_metas = {}
+    node_names = []
+    for node in module.graph.nodes:
+        node_names.append(str(node.name))
+        node_metas[str(node.name)] = NodeMetaSerializer.serialize(node.meta)
+    module._graphmodule_cls_name = module.__class__.__name__
+    return SerializedAOTGraphModule(module, node_metas, node_names)
+
+
+def deserialize_graph_module(module: SerializedAOTGraphModule) -> torch.fx.GraphModule:
+    """Deserialize a graph module, going through each node and updating old names and meta fields
+    TODO: this relies on the fact that the node list order is the same across serialization. How to improve?
+    """
+    new_module = module.module
+    node_metas = module.node_metas
+    for old_name, node in zip(module.node_names, new_module.graph.nodes):
+        node._rename(old_name)
+        node.meta = NodeMetaSerializer.deserialize(node_metas[str(node.name)])
+    new_module.recompile()
+    return new_module
