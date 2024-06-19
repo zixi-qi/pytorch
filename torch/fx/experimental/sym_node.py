0d21b89e8b27326d612fb3d76041a8f19a375dfb
Aaron Orenstein <aorenste@fb.com>
2024-06-18 22:58:26 -0700

    __eq__ and __hash__ for SymNode
    ghstack-source-id: 5b70cc126d5c54d5798b8cfd743250552948b98b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128916

--- torch/fx/experimental/sym_node.py
+++ torch/fx/experimental/sym_node.py
@@ -16,7 +16,7 @@ import math
 import operator
 import sys
 from functools import lru_cache, update_wrapper
-from typing import Optional, Type, TYPE_CHECKING, Union
+from typing import FrozenSet, Optional, Type, TYPE_CHECKING, Union
 
 import torch
 
@@ -126,6 +126,19 @@ class SymNode:
         return SymNode(
             self._expr, shape_env, self.pytype, self._hint, self.constant, self.fx_node
         )
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, SymNode)
+            and self._expr == other._expr
+            and id(self.shape_env) == id(other.shape_env)
+            and self.pytype == other.pytype
+            and self._hint == other._hint
+            and self.constant == other.constant
+        )
+
+    def __hash__(self) -> int:
+        return hash((self._expr, self.pytype, self._hint, self.constant))
 
     @property
     def expr(self):
@@ -483,6 +496,13 @@ class SymNode:
 
     def is_constant(self):
         return False
+
+    def symbols(self) -> FrozenSet["SymNode"]:
+        return frozenset(
+            SymNode(atom, self.shape_env, self.pytype, None)
+            for atom in self._expr.atoms()
+            if atom.is_symbol
+        )
 
 
 # TODO: this probably needs the sizes-strides eval functions
