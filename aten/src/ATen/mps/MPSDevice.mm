8f063b96c87761084801b94cbcdc292bc5dfe765
Denis Vieriu <dvieriu@apple.com>
2024-07-15 00:45:19 -0700

    Add quant changes

--- aten/src/ATen/mps/MPSDevice.mm
+++ aten/src/ATen/mps/MPSDevice.mm
@@ -118,6 +118,7 @@ bool MPSDevice::isMacOS13Plus(MacOSVersion version) const {
   static bool _macos_13_3_plus = [compileOptions respondsToSelector:@selector(maxTotalThreadsPerThreadgroup)] == YES;
 
   static bool _macos_14_0_plus = [mpsCD instancesRespondToSelector:@selector(conjugateWithTensor:name:)] == YES;
+  static bool _macos_15_0_plus = [compileOptions respondsToSelector:@selector(fastMathEnabled)] == YES;
 
   switch (version) {
     case MacOSVersion::MACOS_VER_13_0_PLUS:
@@ -130,6 +131,8 @@ bool MPSDevice::isMacOS13Plus(MacOSVersion version) const {
       return _macos_13_3_plus;
     case MacOSVersion::MACOS_VER_14_0_PLUS:
       return _macos_14_0_plus;
+    case MacOSVersion::MACOS_VER_15_0_PLUS:
+      return _macos_15_0_plus;
     default:
       return false;
   }
