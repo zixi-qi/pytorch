369ac7c12d9a9a393eb3d346cce10c7039fa2414
Mitchell, Frost <frost.mitchell@intel.com>
2024-07-17 10:11:38 -0700

    Fix typing error

--- torch/_inductor/codegen/cpp_gemm_template.py
+++ torch/_inductor/codegen/cpp_gemm_template.py
@@ -1,5 +1,5 @@
 # mypy: allow-untyped-defs
-from typing import Any, Callable, cast, List, Optional, Union
+from typing import Any, Callable, cast, Dict, List, Optional, Union
 from functools import lru_cache
 
 import torch
@@ -504,7 +504,7 @@ class CppPackedGemmTemplate(CppTemplate):
         template_buffer_node: Optional[ir.CppTemplateBuffer] = None,
         epilogue_nodes: Optional[List[ir.IRNode]] = None,
         **kwargs,
-    ) -> dict[str, Any]:
+    ) -> Dict[str, Any]:
         assert len(self.input_nodes) >= 2
 
         int8_gemm = self.input_nodes[0].get_dtype() == torch.uint8
