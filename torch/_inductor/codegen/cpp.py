3c4a8ade748e90b13943c7e6d97e344c05f85d8b
Sun, Jiayi <jiayi.sun@intel.com>
2024-07-18 02:20:22 -0700

    [Inductor] support masked vectorization for the tail_loop for integer datatypes and bool datatype
    ghstack-source-id: 3c7118c88f08b8c763e65bb01180dcfbc75edfd8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128802

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -2976,13 +2976,6 @@ class CppVecKernelChecker(CppVecKernel):
             torch.int64,
         ]
 
-        # TODO: remove it after all data types support masked vectorization.
-        self.supported_dtypes_for_masked_vec: List[torch.dtype] = [
-            torch.float,
-            torch.bfloat16,
-            torch.float16,
-        ]
-
     def disable_vec(self, msg=None):
         if schedule_log.isEnabledFor(logging.DEBUG):
             schedule_log.debug("Disabled vectorization: %s", msg)
@@ -3003,11 +2996,6 @@ class CppVecKernelChecker(CppVecKernel):
             opt_ctx.dtype = load_dtype
             var = self.cse.newvar()
 
-            if load_dtype not in self.supported_dtypes_for_masked_vec:
-                self.disable_masked_vec(
-                    f"{load_dtype} not supported by masked vectorization"
-                )
-
             if has_free_symbols(self.ranges):
                 self.disable_masked_vec("Symbolic ranges not supported by masked load")
 
@@ -3027,11 +3015,6 @@ class CppVecKernelChecker(CppVecKernel):
     def store(self, name, index, value, mode=None):
         with RecordOptimizationContext(__name__) as node_ctx:
             store_dtype = V.graph.get_dtype(name)
-
-            if store_dtype not in self.supported_dtypes_for_masked_vec:
-                self.disable_masked_vec(
-                    f"{store_dtype} not supported by masked vectorization"
-                )
 
             if has_free_symbols(self.ranges):
                 self.disable_masked_vec("Symbolic ranges not supported by masked store")
@@ -3161,11 +3144,6 @@ class CppVecKernelChecker(CppVecKernel):
                         ):
                             opt_ctx.dtype = torch.float32
 
-                    if opt_ctx.dtype not in self.supported_dtypes_for_masked_vec:
-                        self.disable_masked_vec(
-                            f"{opt_ctx.dtype} not supported by masked vectorization"
-                        )
-
                     if opt_ctx.dtype not in self.supported_dtypes:
                         self.disable_vec(f"constant dtype: {opt_ctx.dtype}")
                     return val
@@ -3239,11 +3217,6 @@ class CppVecKernelChecker(CppVecKernel):
 
             @staticmethod
             def to_dtype(x, dtype, src_dtype=None):
-                if dtype not in self.supported_dtypes_for_masked_vec:
-                    self.disable_masked_vec(
-                        f"{dtype} not supported by masked vectorization"
-                    )
-
                 if dtype not in self.supported_dtypes:
                     self.disable_vec(f"to_dtype: {dtype}")
                 return x
